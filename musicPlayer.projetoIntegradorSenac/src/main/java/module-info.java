9ce3ebb022a1ef0d4a9b419a62e298843bde2fd0
Higor Valuar <168334690+higorvaluar@users.noreply.github.com>
2024-06-20 16:01:56 -0300

    Deletando o diretório para adicionar outro

--- musicPlayer.projetoIntegradorSenac/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.musicplayer {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires org.kordamp.bootstrapfx.core;
-
-    opens com.example.musicplayer to javafx.fxml;
-    exports com.example.musicplayer;
-}
